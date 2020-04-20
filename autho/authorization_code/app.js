b11996b746894d1dc1266066094c90cc68b25674
Kaio Luan <kaioluanro@hotmail.com>
2020-04-20 17:17:18 -0300

    reference client

--- autho/authorization_code/app.js
+++ autho/authorization_code/app.js
@@ -13,6 +13,8 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
+var client_id = 'Your client_id'; // Your client id
+var client_secret = 'Your client_secret'; // Your secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
